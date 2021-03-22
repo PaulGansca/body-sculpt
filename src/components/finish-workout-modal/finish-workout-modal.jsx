99303e204942c15bd5381f55b835cf1dd3b43591
Paul Mihai Gansca <gansca.paul@gmail.com>
2021-03-22 12:53:27 +0200

    show workout summary when completed

--- src/components/finish-workout-modal/finish-workout-modal.jsx
+++ src/components/finish-workout-modal/finish-workout-modal.jsx
@@ -4,7 +4,7 @@ import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 
-import { PlayCircleOutlined, CheckCircleOutlined } from '@ant-design/icons';
+import { PlayCircleOutlined, CheckCircleOutlined, InfoCircleOutlined } from '@ant-design/icons';
 
 import CustomModal from '../antd/custom-modal/custom-modal';
 import CustomButton from '../antd/custom-button/custom-button';
@@ -51,12 +51,19 @@ const FinishWorkoutModal = ({workoutState, toggleWorkoutState, weight, exercises
                     let state = workoutState === "not started" || workoutState === "paused" ? "in progress" : "paused";
                     toggleWorkoutState(state)
                 }}>
-                {workoutState === "not started" ? "Start Workout" : "Finish Workout"}</CustomButton> : <></>}
-            <CustomModal visible={isModalVisible} onOk={() => handleOk()}
+                {workoutState === "not started" ? "Start Workout" : "Finish Workout"}</CustomButton> :
+                <CustomButton className={`toggle-state-btn ${btnClass}`} onClick={() => setIsModalVisible(true)}
+                    icon={<InfoCircleOutlined />} shape={"round"}>View Summary</CustomButton>}
+            {workoutState !== "complete" ? <CustomModal visible={isModalVisible} onOk={() => handleOk()}
                 onCancel={() => {toggleWorkoutState("in progress"); setIsModalVisible(false)}} cancelText="Resume Workout" okText="Log Workout">
                 <p>Finish and log your workout?</p>
                 <WorkoutSummary timeElapsed={timeElapsed} exercises={exercises} weight={weight} />
-            </CustomModal>
+            </CustomModal> :
+            <CustomModal visible={isModalVisible} footer={null}
+                onCancel={() => setIsModalVisible(false)} okText="Log Workout">
+                <p>Workout details at a glance:</p>
+                <WorkoutSummary timeElapsed={timeElapsed} exercises={exercises} weight={weight} />
+            </CustomModal>}
         </>
     )
 };
