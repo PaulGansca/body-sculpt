86a9c9c1f7c5ec920350c797882b2461b00a291b
Paul Mihai Gansca <gansca.paul@gmail.com>
2021-04-20 19:08:51 +0300

    hide firebase credentials

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-const config = {
-    apiKey: "AIzaSyDEm02SVxBhYLB3-8ituaNPZtNObjR8eBE",
-    authDomain: "body-sculpt-5ab47.firebaseapp.com",
-    projectId: "body-sculpt-5ab47",
-    storageBucket: "body-sculpt-5ab47.appspot.com",
-    messagingSenderId: "717646268019",
-    appId: "1:717646268019:web:7b8084909e590e6b8c0c06",
-    measurementId: "G-JN2LX5QRQT"
-}
+import config from './firebase-config';
 
 firebase.initializeApp(config);
 
