e2a9d02c6cb78a0804120ed470d8bebf2fb839e3
Paul Mihai Gansca <gansca.paul@gmail.com>
2021-04-25 00:25:51 +0300

    include firebase config

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDEm02SVxBhYLB3-8ituaNPZtNObjR8eBE",
+    authDomain: "body-sculpt-5ab47.firebaseapp.com",
+    projectId: "body-sculpt-5ab47",
+    storageBucket: "body-sculpt-5ab47.appspot.com",
+    messagingSenderId: "717646268019",
+    appId: "1:717646268019:web:7b8084909e590e6b8c0c06",
+    measurementId: "G-JN2LX5QRQT"
+}
+
+export default config;
